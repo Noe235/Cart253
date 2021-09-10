d4be0f1415e7283777520625cd03b985f34f8850
Noe235 <sannoemie@outlook.com>
2021-09-10 13:23:20 -0400

    Done

--- Tutorials/Week2 drawing shapes/js/script.js
+++ Tutorials/Week2 drawing shapes/js/script.js
@@ -11,7 +11,21 @@ Here is a description of this template p5 project.
 //
 // Description of setup() goes here.
 function setup() {
-  background(255, 0, 0);
+  createCanvas (640, 480);
+
+  background(200, 200, 200);
+/*
+  line(0, 0, 500, 500)
+
+  rect(200, 200, 100, 100)
+
+  ellipse(250,80,50,100);
+
+*/
+//drawingmode change test
+  rectMode(CENTER);
+  rect(250, 250, 100,100);
+
 
 }
 
