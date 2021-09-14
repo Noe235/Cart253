f6d02ea0789697072d5f3d300cc25038c4fe6354
Noe235 <sannoemie@outlook.com>
2021-09-14 13:28:12 -0400

    Activity 02: Started the draw an alien activity

--- Tutorials/Week2 drawing shapes/README.md
+++ Tutorials/Week2 drawing shapes/README.md
@@ -1,6 +1,12 @@
 # Template p5 project
-
+//17september
 This is the README file for the entire project. For more official projects you should write information here about the nature of the project, your name, any special explanations of how the project works, etc.
 
 Didn't do much really disn't feel liek experimenting much
 BYEEEEEEEEEEEEEEE
+
+//12september
+Meh don't feel like coloring ...
+Next
+Comments done (not really any need to know since i already do so \\0w0//)
+Next
--- Tutorials/Week2 drawing shapes/js/script.js
+++ Tutorials/Week2 drawing shapes/js/script.js
@@ -2,9 +2,9 @@
 
 /**************************************************
 Template p5 project
-Pippin Barr
+Noemie
 
-Here is a description of this template p5 project.
+doing the tutorials for week 2
 **************************************************/
 
 // setup()
@@ -14,17 +14,34 @@ function setup() {
   createCanvas (640, 480);
 
   background(200, 200, 200);
-/*
+
   line(0, 0, 500, 500)
 
   rect(200, 200, 100, 100)
 
   ellipse(250,80,50,100);
 
-*/
+
 //drawingmode change test
   rectMode(CENTER);
   rect(250, 250, 100,100);
+
+//making a cone
+  noStroke();
+  fill(243,49,62)
+  ellipseMode(CORNER);
+
+  ellipse(250, 350, 100, 100);
+
+  ellipse(250,350,80,80);
+
+  ellipse(250,350,60,60);
+
+  ellipse(250,350,40,40);
+
+  ellipse(250,350,20,20);
+    fill(0,0,25);
+
 
 
 }
