033bab79d83bca5dfe3b730c7f42372d0de85bd6
Noe235 <sannoemie@outlook.com>
2021-09-17 11:26:09 -0400

    playing a little at the same time as the teacher

--- activities/02-monalisa/js/script.js
+++ activities/02-monalisa/js/script.js
@@ -1,17 +1,47 @@
 "use strict";
 
 /**************************************************
-Template p5 project
-Pippin Barr
+Mona Lisa recreation
+Noemie
 
-Here is a description of this template p5 project.
+let's do a mona lisa
 **************************************************/
 
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
-  background(255, 0, 0);
+  createCanvas(400, 600);
+
+  background(144, 209, 152);
+
+  //hair
+  fill(50, 10, 10);
+  ellipse(210, 180, 180, 240)
+  //ellipse( 205, 230, 150, 200 )
+
+  //face
+  noStroke();
+  fill(219, 195, 114);
+  ellipse(200, 150, 110, 160);
+  //  ellipse( 200, 200, 100, 120 );
+
+  //mouth attempt
+  noFill();
+  stroke(0);
+  strokeWeight(10);
+  //arc( 50, 55, 50, 50, 0, HALF_PI );
+  line(180, 200, 220, 200)
+
+
+
+  //hair
+
+
+  //face
+
+
+
 
 }
 
