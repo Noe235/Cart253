83cb0d283311f562f8d55632e2cd66550b2bc592
Noe235 <sannoemie@outlook.com>
2021-09-23 10:30:41 -0400

    E1: Started exercise 1

---
+++ Exercises/i-like-to-move-it/js/script.js
@@ -0,0 +1,32 @@
+/**
+I Like to move it
+Noemie
+
+
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
