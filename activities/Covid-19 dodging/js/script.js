c4b3276fd2f5d294a502e0797ec953ea1fbf2c7d
Noe235 <sannoemie@outlook.com>
2021-09-30 16:41:25 -0400

    finished, change to rebound everywhere +2 covid

--- activities/Covid-19 dodging/js/script.js
+++ activities/Covid-19 dodging/js/script.js
@@ -20,7 +20,23 @@ let covid19 = {
   x: 0,
   y: 0,
   vx: 0.1,
-  d: 100
+  vy: 0.2,
+  d: 100,
+
+}
+let covid219 = {
+  x: 100,
+  y: 500,
+  vx: 0.1,
+  vy: 0.2,
+  d: 100,
+}
+let covid319 = {
+  x: 500,
+  y: 700,
+  vx: 0.3,
+  vy: 0.2,
+  d: 100,
 
 }
 
@@ -32,23 +48,31 @@ let player = {
 
 
 /**
-Description of setup
+get all the ramdon of only one time ready
 */
 function setup() {
   createCanvas(900, 900);
   covid19.y = random(90, 650);
   covid19.vx = random(0.1, 10);
+  covid19.vy = random(0.1, 10);
+  covid219.y = random(90, 650);
+  covid219.vx = random(0.1, 10);
+  covid219.vy = random(0.1, 10);
+  covid319.y = random(90, 650);
+  covid319.vx = random(0.1, 10);
+  covid319.vy = random(0.1, 10);
 }
 
 
 
 /**
-Description of draw()
+Covid,player,bg,static, rules
 */
 function draw() {
   console.log(covid19.d);
   push();
   background(bg);
+
 
   for (statics.n = 0; statics.n < 1000; statics.n++) {
     statics.x = random(0, width);
@@ -58,10 +82,11 @@ function draw() {
   }
 
 
+
   //initial circle/player
   pop()
   fill(0, 0, 255);
-  circle(player.x, player.y, 90);
+  circle(player.x, player.y, 70);
 
 
   //constrain of player
@@ -72,27 +97,67 @@ function draw() {
   pop()
   fill(255, 0, 0);
   covid19.x += covid19.vx;
+  covid19.y += covid19.vy;
+  covid19.nf = random(1, 10);
   circle(covid19.x, covid19.y, 90);
-
   //bouncing rule
   if (covid19.x - 50 > width || covid19.x + 50 < 0) {
     covid19.vx = -covid19.vx;
+
   }
-  // if (covid19.x < 0) {
-  //   covid19.x += covid19.vx;
-  // }
+  if (covid19.y > height || covid19.y < 0) {
+    covid19.vy = -covid19.vy;
+  }
+
+  //covid219
+  covid219.x += covid219.vx;
+  covid219.y += covid219.vy;
+  circle(covid219.x, covid219.y, 90);
+
+  //bouncing rule
+  if (covid219.x - 50 > width || covid219.x + 50 < 0) {
+    covid219.vx = -covid219.vx;
+
+  }
+  if (covid219.y > height || covid219.y < 0) {
+    covid219.vy = -covid219.vy;
+
+  }
+
+  //covid319
+  covid319.x += covid319.vx;
+  covid319.y += covid319.vy;
+  circle(covid319.x, covid319.y, 90);
+
+  //bouncing rule
+  if (covid319.x - 50 > width || covid319.x + 50 < 0) {
+    covid319.vx = -covid319.vx;
+
+  }
+  if (covid319.y > height || covid319.y < 0) {
+    covid319.vy = -covid319.vy;
+
+  }
 
 
 
-
-  //level next
-
-  //counter
 
   //gameover
   covid19.d = dist(player.x, player.y, covid19.x, covid19.y)
-  if (covid19.d < 90)
+  if (covid19.d < 75) {
     noLoop()
+  }
+  covid219.d = dist(player.x, player.y, covid219.x, covid219.y)
+  if (covid219.d < 75) {
+    noLoop()
+  }
+
+  covid319.d = dist(player.x, player.y, covid319.x, covid319.y)
+  if (covid319.d < 75) {
+    noLoop()
+  }
+
+
 
 }
 
