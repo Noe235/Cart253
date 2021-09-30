a4d20d908acb7f2ae7066473bdae000e761c4cb1
Noe235 <sannoemie@outlook.com>
2021-09-30 13:48:09 -0400

    Setup description of the project

---
+++ activities/Covid-19 dodging/js/script.js
@@ -0,0 +1,32 @@
+/**
+Covid 19 dodging
+Noemie
+
+dodging activity
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
