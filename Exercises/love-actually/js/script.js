5f9b3e22b65acae6419e989715d9fe85b38f3a53
Noe235 <sannoemie@outlook.com>
2021-10-06 17:14:48 -0400

    controls+player+1friend+bg

--- Exercises/love-actually/js/script.js
+++ Exercises/love-actually/js/script.js
@@ -7,12 +7,33 @@ Noemie
 "use strict";
 
 
+
+let bgnormal = 0
+let bglove = 0
+
+let player = {
+  x: 30,
+  y: 450,
+  size: 50,
+  vx: 0.1,
+  yx: 0.1,
+  speed: 2
+}
+
+let friend = {
+  x: 500,
+  y: 400,
+  size: 50,
+  vx: 0.1,
+  yx: 0.1,
+  speed: 2
+}
 /**
 Description of preload
 */
 function preload() {
-  let bgnormal = loadImage(`assets\images\greenlandscape.jpg`);
-  let bglove = loadImage(`assets\images\mountain.jpg`);
+  bgnormal = loadImage(`assets/images/greenlandscape.jpg`);
+  bglove = loadImage(`assets/images/mountain.jpg`);
 }
 
 
@@ -20,7 +41,10 @@ function preload() {
 Description of setup
 */
 function setup() {
-  createCanvas(800, 900);
+  createCanvas(900, 500);
+  background(23);
+
+
 }
 
 
@@ -28,5 +52,41 @@ function setup() {
 Description of draw()
 */
 function draw() {
+  image(bgnormal, 0, 0, width, height);
 
+  //player
+  noStroke();
+  fill(0);
+  circle(player.x, player.y, player.size);
+
+  //npc
+  fill(235, 192, 23)
+  circle(friend.x, friend.y, friend.size);
+
+
+
+
+
+  //player command
+  if (keyIsDown(LEFT_ARROW)) {
+    player.vx = -player.speed;
+  } else if (keyIsDown(RIGHT_ARROW)) {
+    player.vx = player.speed;
+  } else {
+    player.vx = 0;
+  }
+
+  if (keyIsDown(UP_ARROW)) {
+    player.vy = -player.speed;
+  } else if (keyIsDown(DOWN_ARROW)) {
+    player.vy = player.speed;
+  } else {
+    player.vy = 0;
+  }
+  player.x = player.x + player.vx;
+  player.y = player.y + player.vy;
+
+
+  //log for debug
+  console.log(player.x, player.y)
 }
