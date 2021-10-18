c3f39a51c0aee09bc46fb87a86fb93a99c924256
Noe235 <sannoemie@outlook.com>
2021-10-18 18:37:59 -0400

    menu set-up

--- Project 1/js/script.js
+++ Project 1/js/script.js
@@ -8,12 +8,18 @@ sheep game where you regroup the Sheep
 **************************************************/
 
 let gamescreen = `menu`; //(menu, game)
-let gamestate = `playing`; //(playing, pause)
-let gamepause = `npause`; //(npause, option)
+let gamestate = `playing`; //(playing, pause,)
+let gamepause = `npause`; //(pause, option,cedit)
+let gameoverlay = `no` //(no, gpause,option,credit)
 let menuImage = `assets/images/Menu/menuImage.jpg`;
 
-let button = [];
-
+// let buttonPlay(){
+//   x:
+//   y:
+//   sizex:
+//   sizey:
+//
+// }
 function preload() {
   menuImage = loadImage(`assets/images/Menu/menuImage.jpg`);
 
@@ -24,21 +30,106 @@ function preload() {
 function setup() {
   createCanvas(1100, 800);
   background(51);
+  if (gamescreen === `menu`) {
+    menu();
 
+  }
 }
 
 // draw()
 //
 // Description of draw() goes here.
 function draw() {
-  if (gamescreen === `menu`) {
-    image(menuImage, 0, 0, width, height);
+
+
+  if (gamescreen === `game`) {
+    clear();
+    text(`insert game here`, width / 2, height / 2);
+
 
   }
+
+
+  console.log(mouseX, mouseY)
 }
 
-
 function menu() {
+  image(menuImage, 0, 0, width, height);
+  button();
 
+  //Menu text
+  textSize(51);
+  textAlign(CENTER);
+  fill(255);
+  text(`Play`, width / 2, 285);
+  text(`Options`, width / 2, 420);
+  text(`Credit`, width / 2, 550);
+
+
+
+}
+
+function button() {
+  rectMode(CENTER);
+  noStroke();
+  fill(37, 142, 112); //pomu color
+  rect(width / 2, height / 3, 400, 70);
+  rect(width / 2, height / 2, 400, 70);
+  rect(width / 2, height * 2 / 3, 400, 70);
+
+}
+
+function mousePressed() {
+  if (gamestate === `playing`) {
+    if (gamescreen === `menu`)
+      //play game
+      if ((mouseX > 550 - 200) && (mouseX < 550 + 200) &&
+        (mouseY > 266 - 35) && (mouseY < 266 + 35)) {
+        gamescreen = `game`
+
+      }
+    //option screen
+    if ((mouseX > 550 - 200) && (mouseX < 550 + 400) &&
+      (mouseY > 400 - 35) && (mouseY < 400 + 35)) {
+      fill(0, 0, 0, 170)
+      rect(width / 2, height / 2, 900, 500, 20);
+      noStroke();
+      fill(37, 142, 112); //X buton
+      rect(950, 200, 50, 50, 5);
+      textSize(51);
+      textAlign(CENTER);
+      fill(255);
+      text(`X`, 950, 220);
+      gamestate = `pause`;
+      gameoverlay = `option`;
+
+    }
+    //cedit screen
+    if ((mouseX > 550 - 200) && (mouseX < 550 + 400) &&
+      (mouseY > 532 - 35) && (mouseY < 532 + 35)) {
+      fill(0, 0, 0, 170)
+      rect(width / 2, height / 2, 900, 500, 20);
+      noStroke();
+      fill(37, 142, 112); //X button
+      rect(950, 200, 50, 50, 5);
+      textSize(51);
+      textAlign(CENTER);
+      fill(255);
+      text(`X`, 950, 220);
+      gamestate = `pause`;
+      gameoverlay = `cedit`;
+
+    }
+  }
+  //interaction of overlay
+  if (gamestate === `pause`) {
+    if ((mouseX > 950 - 25) && (mouseX < 950 + 25) &&
+      (mouseY > 200 - 25) && (mouseY < 200 + 25)) {
+      menu();
+      gamestate = `playing`;
+    }
+
+
+  }
 
 }
