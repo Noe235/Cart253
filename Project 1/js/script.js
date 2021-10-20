0757e76e8e1dd7f733587e94738934ff5d1f5319
Noe235 <sannoemie@outlook.com>
2021-10-20 14:10:01 -0400

    teach inspecting code because sound not working

--- Project 1/js/script.js
+++ Project 1/js/script.js
@@ -12,19 +12,16 @@ let gamestate = `playing`; //(playing, pause,)
 let gamepause = `npause`; //(pause, option,cedit)
 let gameoverlay = `no` //(no, gpause,option,credit)
 let menuImage = `assets/images/Menu/menuImage.jpg`;
-let mVolume = 0; //slider for volume
+let mVolume = 0.5; //slider for volume
+let musicPlaying = `false`;
 let musiccolor = 10;
 let track = `1`; //number adds a red dot
+let track1 = `assets/sounds/Trial and Error.mp3`;
+let track2 = `assets/sounds/bark.wav`;
 let d1 = 50;
 let d2 = 50;
 let d3 = 50;
 let d4 = 50;
-let musicH = {
-  o1: 100,
-  o2: 100,
-  o3: 100,
-  o4: 100,
-};
 
 
 // let buttonPlay(){
@@ -36,7 +33,8 @@ let musicH = {
 // }
 function preload() {
   menuImage = loadImage(`assets/images/Menu/menuImage.jpg`);
-
+  track1 = loadSound(`assets/sounds/Trial and Error.mp3`);
+  track2 = loadSound(`assets/sounds/bark.wav`);
 }
 // setup()
 //#258E70
@@ -76,6 +74,8 @@ function draw() {
   }
 
 
+
+
   console.log(d1)
 }
 
@@ -91,14 +91,15 @@ function menu() {
   text(`Play`, width / 2, 285);
   text(`Options`, width / 2, 420);
   text(`Credit`, width / 2, 550);
-  if (mVolume < 0) {
-    mVolume.hide();
-  }
+  // if (mVolume < 0) {
+  //   mVolume.hide();
+  // }
 
 
 
 }
 
+//menu buttons
 function button() {
   rectMode(CENTER);
   noStroke();
@@ -127,93 +128,94 @@ function option() {
 
   //music selector
 
-  push();
-
 
   //track 1
-  fill(149, 200, 216, musicH.o1);
+  fill(149, 200, 216, 100);
   rect(220, 350, 190, 310, 20);
   fill(149, 200, 216, 120);
   circle(217, 470, 60);
-  push();
+
   fill(255);
   triangle(210, 450,
     235, 470,
     210, 490);
-  pop();
 
-  push();
   fill(255);
   textAlign(LEFT);
   textSize(16);
   text(`insert music name here`, 135, 420);
-  pop();
+
 
   //track 2
-  fill(149, 200, 216, musicH.o2);
+  fill(149, 200, 216, 100);
   rect(420, 350, 190, 310, 20);
   fill(149, 200, 216, 120);
   circle(417, 470, 60);
-  push();
+
   fill(255);
   triangle(410, 450,
     435, 470,
     410, 490);
-  pop();
-  push();
+
   fill(255);
   textAlign(LEFT);
   textSize(16);
   text(`insert music name here`, 335, 420);
-  pop();
+
 
   //track 3
-  fill(149, 200, 216, musicH.o3);
+  fill(149, 200, 216, 100);
   rect(620, 350, 190, 310, 20);
   fill(149, 200, 216, 120);
   circle(617, 470, 60);
-  push();
+
   fill(255);
   triangle(610, 450,
     635, 470,
     610, 490);
-  pop();
-  push();
+
   fill(255);
   textAlign(LEFT);
   textSize(16);
   text(`insert music name here`, 535, 420);
-  pop();
+
 
   //track 4
-  fill(149, 200, 216, musicH.o4);
+  fill(149, 200, 216, 100);
   rect(820, 350, 190, 310, 20);
   fill(149, 200, 216, 120);
   circle(817, 470, 60);
-  push();
+
   fill(255);
   triangle(810, 450,
     835, 470,
     810, 490);
-  pop();
-  push();
+
   fill(255);
   textAlign(LEFT);
   textSize(16);
   text(`insert music name here`, 735, 420);
-  pop();
-  pop();
-  //music sound adjusment
 
+
+  //music sound adjusment
+  fill(255);
   textAlign(LEFT);
   textSize(40);
   text(`Volume`, 150, 550);
-  mVolume = createSlider(0, 5, 10, 0);
-  mVolume.position(150, 650);
-  mVolume.style(`width`, `800px`);
+  // mVolume = createSlider(0, 1, 10, 0);
+  // mVolume.position(150, 650);
+  // mVolume.style(`width`, `800px`);
 
 
 
+}
+
+function musicPlayer() {
+
+  if (musicPlaying === `false`) {
+    track2.play()
+    musicPlaying = `true`;
+  }
 }
 
 function credit() {
@@ -233,7 +235,7 @@ function credit() {
   //cedit text
   textAlign(LEFT);
   textSize(20)
-  text(`insert text here`, 150, 300);
+  text(`Menu background by brgfx -www.freepik.com`, 150, 300);
   text(`insert text here`, 150, 350);
   text(`insert text here`, 150, 400);
   text(`insert text here`, 150, 450);
@@ -250,32 +252,33 @@ function musicChecker() {
   d4 = dist(mouseX, mouseY, 817, 470);
 
   if (track === `1`) {
-    push();
+
     fill(255, 0, 0);
     circle(270, 450, 20);
-    pop();
+
+
   } else if (track === `2`) {
-    push();
+
     fill(255, 0, 0);
     circle(470, 450, 20);
-    pop();
+
   } else if (track === `3`) {
-    push();
+
     fill(255, 0, 0);
     circle(670, 450, 20);
     pop();
   } else if (track === `4`) {
-    push();
+
     fill(255, 0, 0);
     circle(870, 450, 20);
-    pop();
+
   }
 }
 
 function musicSelector() {
   if (d1 < 30) {
-
     track = `1`;
+    // musicPlayer();
   }
   if (d2 < 30) {
     track = `2`;
@@ -290,42 +293,47 @@ function musicSelector() {
 }
 
 function mousePressed() {
-  if (gamestate === `playing`) {
-    if (gamescreen === `menu`)
-      //play game
-      if ((mouseX > 550 - 200) && (mouseX < 550 + 200) &&
-        (mouseY > 266 - 35) && (mouseY < 266 + 35)) {
-        gamescreen = `game`
+  // track1.play();
 
-      }
-    //option screen
-    if ((mouseX > 550 - 200) && (mouseX < 550 + 400) &&
-      (mouseY > 400 - 35) && (mouseY < 400 + 35)) {
-      gameoverlay = `option`
-
-    }
-    //cedit screen
-    if ((mouseX > 550 - 200) && (mouseX < 550 + 400) &&
-      (mouseY > 532 - 35) && (mouseY < 532 + 35)) {
-      gameoverlay = `credit`;
-    }
-  }
-  //interaction of overlay
-  //close button
-  if (gamestate === `pause`) {
-    if ((mouseX > 950 - 25) && (mouseX < 950 + 25) &&
-      (mouseY > 200 - 25) && (mouseY < 200 + 25)) {
-      menu();
-      gamestate = `playing`;
-      gameoverlay = `no`;
-    }
-
-    // music selector
-    if (gameoverlay === `option`) {
-      musicSelector();
+  track2.play();
 
 
-    }
-  }
+  // if (gamestate === `playing`) {
+  //   if (gamescreen === `menu`)
+  //     //play game
+  //     if ((mouseX > 550 - 200) && (mouseX < 550 + 200) &&
+  //       (mouseY > 266 - 35) && (mouseY < 266 + 35)) {
+  //       gamescreen = `game`
+  //
+  //     }
+  //   //option screen
+  //   if ((mouseX > 550 - 200) && (mouseX < 550 + 400) &&
+  //     (mouseY > 400 - 35) && (mouseY < 400 + 35)) {
+  //     gameoverlay = `option`
+  //
+  //   }
+  //   //cedit screen
+  //   if ((mouseX > 550 - 200) && (mouseX < 550 + 400) &&
+  //     (mouseY > 532 - 35) && (mouseY < 532 + 35)) {
+  //     gameoverlay = `credit`;
+  //   }
+  // }
+  // //interaction of overlay
+  // //close button
+  // if (gamestate === `pause`) {
+  //   if ((mouseX > 950 - 25) && (mouseX < 950 + 25) &&
+  //     (mouseY > 200 - 25) && (mouseY < 200 + 25)) {
+  //     menu();
+  //     gamestate = `playing`;
+  //     gameoverlay = `no`;
+  //   }
+  //
+  //   // music selector
+  //   if (gameoverlay === `option`) {
+  //     musicSelector();
+  //
+  //
+  //   }
+  // }
 
 }
