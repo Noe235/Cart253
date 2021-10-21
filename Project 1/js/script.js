047325786daceec0122502784b4845fcf8e70277
Noe235 <sannoemie@outlook.com>
2021-10-21 15:38:48 -0400

    bare bone of the game movement with mouse check, left= game over

--- Project 1/js/script.js
+++ Project 1/js/script.js
@@ -25,10 +25,17 @@ let track2 = `assets/sounds/bark.wav`;
 
 
 //distances
+//music
 let d1 = 50;
 let d2 = 50;
 let d3 = 50;
 let d4 = 50;
+
+//game
+let dragoons = [];
+let flock = 5;
+let dragooneggs = [];
+let eggs = 3;
 
 //arrays looping
 // let atrackrectx= [220,420,620,820];
@@ -54,8 +61,13 @@ function setup() {
   mbutton2 = menubutton(height / 2);
   mbutton3 = menubutton(height * 2 / 3);
 
-  //more stuff go give numbers to
-
+  //making dragoons gving them position
+  for (let i = 0; i < flock; i++) {
+    dragoons[i] = createDragoons(random(20, width), random(20, height));
+  }
+  for (let i = 0; i < eggs; i++) {
+    dragooneggs[i] = createDragooneggs(random(20, width), random(20, height));
+  }
 }
 // draw()
 //
@@ -65,15 +77,14 @@ function draw() {
     if (gameoverlay === `no`) {
       menu();
     }
-
   }
-
-
   //start game
   if (gamescreen === `game`) {
-    clear();
-    text(`insert game here`, width / 2, height / 2);
-
+    // game runining (will check all the distance and the runing stuff)
+    //game set-up
+    gameRuning();
+    gamePieces();
+    checkDist();
 
   }
 
@@ -87,9 +98,137 @@ function draw() {
     credit();
   }
 
+}
+
+function gameRuning() {
+  background(71, 179, 57);
+
+  //pen
+  // fill(255);
+  // rect(200, 400, 130, 130);
+  stroke(89, 66, 51);
+  strokeWeight(4);
+  fill(89, 66, 51);
+  line(135, 335,
+    265, 335);
+  line(135, 335,
+    135, 465);
+  line(135, 465,
+    265, 465);
+
+  //dragoons drawing
+}
+
+function checkDist() {
+  for (let i = 0; i < dragoons.length; i++) {
+    let dis = dist(mouseX, mouseY, dragoons[i].x, dragoons[i].y)
+
+    if (dis < 17) {
+      if (mouseX > dragoons[i].x) { //mouse is on the right
+        dragoons[i].x -= 10
+      }
+      if (mouseX < dragoons[i].x) { //mouse on the left
+        dragoons[i].x += 10
+      }
+      if (mouseY > dragoons[i].y) { //mouse is below
+        dragoons[i].y -= 10
+      }
+      if (mouseY < dragoons[i].y) { //mousse is above
+        dragoons[i].y += 10
+      }
+    }
+  }
+  for (let i = 0; i < dragooneggs.length; i++) {
+    let disegg = dist(mouseX, mouseY, dragooneggs[i].x, dragooneggs[i].y)
+
+    if (disegg < 17) {
+      if (mouseX > dragooneggs[i].x) { //mouse is on the right
+        dragooneggs[i].x -= 10
+      }
+      if (mouseX < dragooneggs[i].x) { //mouse on the left
+        dragooneggs[i].x += 10
+      }
+      if (mouseY > dragooneggs[i].y) { //mouse is below
+        dragooneggs[i].y -= 10
+      }
+      if (mouseY < dragooneggs[i].y) { //mousse is above
+        dragooneggs[i].y += 10
+      }
+    }
+  }
+}
+
+function gamePieces() {
+  for (let i = 0; i < dragoons.length; i++) {
+    displayDragoons(dragoons[i]);
+  }
+
+  for (let i = 0; i < dragooneggs.length; i++) {
+    displayDragooneggs(dragooneggs[i]);
+  }
+  for (let i = 0; i < dragooneggs.length; i++) {
+    moveDragooneggs(dragooneggs[i]);
+  }
+
+}
+
+function createDragoons(x, y) {
+  let dragoons = {
+    x: x,
+    y: y,
+    size: 25,
+  }
+  return dragoons;
+}
+
+function displayDragoons(dragoons) {
+  push();
+  noStroke();
+  fill(126, 78, 172);
+  circle(dragoons.x, dragoons.y, dragoons.size);
+  pop();
+  //constrain the dragoons
+  dragoons.x = constrain(dragoons.x, 20, width);
+  dragoons.y = constrain(dragoons.y, 20, height);
 
 
+}
 
+function createDragooneggs(x, y) {
+  let dragooneggs = {
+    x: x,
+    y: y,
+    size: 35,
+    vx: 0,
+    vy: 0,
+    speed: 0.2
+  }
+  return dragooneggs;
+}
+
+function displayDragooneggs(dragooneggs) {
+  push();
+  noStroke();
+  fill(255);
+  circle(dragooneggs.x, dragooneggs.y, dragooneggs.size);
+  pop();
+
+
+}
+
+function moveDragooneggs(dragooneggs) {
+  let change = random(0, 1);
+  if (change < 0.05) {
+    dragooneggs.vx = random(-dragooneggs.speed, dragooneggs.speed);
+    dragooneggs.vy = random(-dragooneggs.speed, dragooneggs.speed);
+  }
+  //  move the egg
+  dragooneggs.x += dragooneggs.vx;
+  dragooneggs.y += dragooneggs.vy;
+
+  //constrain the eggs
+  dragooneggs.x = constrain(dragooneggs.x, 20, width);
+  dragooneggs.y = constrain(dragooneggs.y, 20, height);
 
 }
 
@@ -151,7 +290,6 @@ function option() {
     trianx1: 210,
     trianx2: 235
   }
-
   for (let rt = 0; rt < 4; rt++) {
     fill(149, 200, 216, 100);
     rect(tdisplay.rectx, 350, 190, 310, 20);
@@ -167,16 +305,6 @@ function option() {
     tdisplay.trianx2 += 200;
   }
   // //track 1
-  // fill(149, 200, 216, 100);
-  // rect(220, 350, 190, 310, 20);
-  // fill(149, 200, 216, 120);
-  // circle(217, 470, 60);
-  //
-  // fill(255);
-  // triangle(210, 450,
-  //   235, 470,
-  //   210, 490);
-  //
   fill(255);
   textAlign(LEFT);
   textSize(16);
@@ -184,16 +312,6 @@ function option() {
 
 
   // //track 2
-  // fill(149, 200, 216, 100);
-  // rect(420, 350, 190, 310, 20);
-  // fill(149, 200, 216, 120);
-  // circle(417, 470, 60);
-  //
-  // fill(255);
-  // triangle(410, 450,
-  //   435, 470,
-  //   410, 490);
-
   fill(255);
   textAlign(LEFT);
   textSize(16);
@@ -201,16 +319,6 @@ function option() {
 
 
   // //track 3
-  // fill(149, 200, 216, 100);
-  // rect(620, 350, 190, 310, 20);
-  // fill(149, 200, 216, 120);
-  // circle(617, 470, 60);
-  //
-  // fill(255);
-  // triangle(610, 450,
-  //   635, 470,
-  //   610, 490);
-
   fill(255);
   textAlign(LEFT);
   textSize(16);
@@ -218,16 +326,6 @@ function option() {
 
 
   // //track 4
-  // fill(149, 200, 216, 100);
-  // rect(820, 350, 190, 310, 20);
-  // fill(149, 200, 216, 120);
-  // circle(817, 470, 60);
-  //
-  // fill(255);
-  // triangle(810, 450,
-  //   835, 470,
-  //   810, 490);
-
   fill(255);
   textAlign(LEFT);
   textSize(16);
@@ -336,28 +434,29 @@ function musicSelector() {
 function mousePressed() {
 
   if (gamestate === `playing`) {
-    if (gamescreen === `menu`)
+    if (gamescreen === `menu`) {
       //play game
       if ((mouseX > 550 - 200) && (mouseX < 550 + 200) &&
         (mouseY > 266 - 35) && (mouseY < 266 + 35)) {
         gamescreen = `game`
 
       }
-    //option screen
-    if ((mouseX > 550 - 200) && (mouseX < 550 + 400) &&
-      (mouseY > 400 - 35) && (mouseY < 400 + 35)) {
-      gameoverlay = `option`
-      //music sound adjusment
+      //option screen
+      if ((mouseX > 550 - 200) && (mouseX < 550 + 400) &&
+        (mouseY > 400 - 35) && (mouseY < 400 + 35)) {
+        gameoverlay = `option`
+        //music sound adjusment
 
-      mVolume = createSlider(0, 1, 0.2, 0.01);
-      mVolume.position(150, 650);
-      mVolume.style(`width`, `800px`);
+        mVolume = createSlider(0, 1, 0.2, 0.01);
+        mVolume.position(150, 650);
+        mVolume.style(`width`, `800px`);
 
-    }
-    //cedit screen
-    if ((mouseX > 550 - 200) && (mouseX < 550 + 400) &&
-      (mouseY > 532 - 35) && (mouseY < 532 + 35)) {
-      gameoverlay = `credit`;
+      }
+      //cedit screen
+      if ((mouseX > 550 - 200) && (mouseX < 550 + 400) &&
+        (mouseY > 532 - 35) && (mouseY < 532 + 35)) {
+        gameoverlay = `credit`;
+      }
     }
   }
   //interaction of overlay
