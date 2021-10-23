2a6bf75ebe0e898ff84a21f8e76d6cf9cc82a520
Noe235 <sannoemie@outlook.com>
2021-10-23 13:54:23 -0400

    alll fish and move interaction works, all left is gameover

--- Exercises/age-of-aquariums/js/script.js
+++ Exercises/age-of-aquariums/js/script.js
@@ -7,19 +7,25 @@ noemie
 let fish = []
 let school = 5
 
-let lilypads = []
+let lilypad = []
 let plants = 5
 
-
+let fishcapture = 0
 // setup()
 //
 // Description of setup() goes here.
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
-  //crete the place of the fishies
+  plants = random(5, 10)
+  school = random(5, 20)
+  //create teh parameters of the fishies
   for (let i = 0; i < school; i++) {
-    fish[i] = createFish(random(0, width), random(0, height));
+    fish[i] = createFish(random(0, width), random(0, height), random(20, 50), random(1, 2.5));
+  }
+
+  //create the place of  the lilypads
+  for (let i = 0; i < plants; i++) {
+    lilypad[i] = createLilypad(random(0, width), random(0, height), random(10, 200));
   }
 }
 
@@ -28,30 +34,105 @@ function setup() {
 // Description of draw() goes here.
 function draw() {
   background(149, 200, 216);
-  enviroment();
+
+
+  for (let i = 0; i < fish.length; i++) {
+    moveFish(fish[i]);
+  }
+  for (let i = 0; i < fish.length; i++) {
+    displayFish(fish[i]);
+  }
+
+
+  for (let i = 0; i < lilypad.length; i++) {
+    displayEnviroment(lilypad[i]);
+  }
+
+  user();
+
+  fishing();
+
 
 }
 
-function createFish(x, y) {
+function createFish(x, y, size, speed) {
   let fish = {
     x: x,
     y: y,
-    size: 2,
+    size: size,
     vx: 0,
     vy: 0,
-    speed: 1,
+    speed: speed,
+    caught: false,
   }
   return fish;
 }
 
-function moveFish() {
-
+function displayFish(fish) {
+  if (!fish.caught) {
+    noStroke();
+    fill(126, 78, 172);
+    circle(fish.x, fish.y, fish.size);
+  }
 }
 
-function enviroment() {
+function moveFish(fish) {
+  let change = random(0, 1);
+  if (change < 0.05) {
+    fish.vx = random(-fish.speed, fish.speed);
+    fish.vy = random(-fish.speed, fish.speed);
+  }
+  //  move the fish
+  fish.x += fish.vx;
+  fish.y += fish.vy;
+
+  //constrain the fish to the canvas
+  fish.x = constrain(fish.x, 30, width);
+  fish.y = constrain(fish.y, 30, height);
+}
+
+function createLilypad(x, y, size) {
+  let lilypad = {
+    x: x,
+    y: y,
+    size: size,
+  }
+  return lilypad;
+}
+
+
+function displayEnviroment(lilypad) {
+  noStroke();
+  fill(37, 142, 112);
+  circle(lilypad.x, lilypad.y, lilypad.size);
+}
+
+function user() {
+  fill(255, 0, 0);
+  circle(mouseX, mouseY, 30);
+  fill(255);
+  circle(mouseX, mouseY, 20);
+}
+
+function fishing() {
+  for (let i = 0; i < fish.length; i++) {
+    let d = dist(mouseX, mouseY, fish[i].x, fish[i].y);
+    if (d < 15 + (fish[i].size / 2)) {
+      fish[i].caught = true
+      fishcapture += 1;
+
+    }
+  }
+}
+
+function remove() {
 
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  //re put the lily pads
+  for (let i = 0; i < plants; i++) {
+    lilypad[i] = createLilypad(random(0, width), random(0, height), random(10, 200));
+  }
 }
