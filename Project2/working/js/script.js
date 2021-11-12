753ef506ee5501bd78203ad39617b990d26eb09c
Noe235 <sannoemie@outlook.com>
2021-11-11 23:48:38 -0500

    P2 prototype Milliecoincatch, start

---
+++ Project2/working/js/script.js
@@ -0,0 +1,31 @@
+/**
+Niji-Games
+Noemie
+
+*/
+
+"use strict";
+
+
+/**
+Description of preload
+*/
+function preload() {
+
+}
+
+
+/**
+Description of setup
+*/
+function setup() {
+
+}
+
+
+/**
+Description of draw()
+*/
+function draw() {
+
+}
